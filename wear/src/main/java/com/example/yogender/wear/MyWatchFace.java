a5e51e919762e89484c0731e84b7b5d1ede0d8e4
yogenders <yogender.singh.wd@gmail.com>
2016-10-08 22:06:47 +0530

    Feat: Sync data 1

--- wear/src/main/java/com/example/yogender/wear/MyWatchFace.java
+++ wear/src/main/java/com/example/yogender/wear/MyWatchFace.java
@@ -35,6 +35,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.wearable.provider.WearableCalendarContract;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
@@ -53,6 +55,8 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.Wearable;
 
 import java.lang.ref.WeakReference;
@@ -106,7 +110,7 @@ public class MyWatchFace extends CanvasWatchFaceService {
         }
     }
 
-    private class Engine extends CanvasWatchFaceService.Engine {
+    private class Engine extends CanvasWatchFaceService.Engine implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
         final Handler mUpdateTimeHandler = new EngineHandler(this);
         boolean mRegisteredTimeZoneReceiver = false;
         Paint mBackgroundPaint;
@@ -114,7 +118,8 @@ public class MyWatchFace extends CanvasWatchFaceService {
         boolean mAmbient;
         Time mTime;
 
-        GoogleApiClient mGoogleApiClient;
+        String LogTag = "Error";
+
 
         // variable for XML layout file
         private int specW, specH;
@@ -122,7 +127,6 @@ public class MyWatchFace extends CanvasWatchFaceService {
         private TextView day, date, minTemp,maxTemp;
         private final Point displaySize = new Point();
         private ImageView icon,pixelIcon;
-        private Context mContext;
 
 
         final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
@@ -163,11 +167,15 @@ public class MyWatchFace extends CanvasWatchFaceService {
             }
         };
 
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(MyWatchFace.this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Wearable.API)
+                .build();
+
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
-
-            mContext = getBaseContext();
 
             setWatchFaceStyle(new WatchFaceStyle.Builder(MyWatchFace.this)
                     .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
@@ -207,28 +215,6 @@ public class MyWatchFace extends CanvasWatchFaceService {
 
 
             mLoadWheatherDataHandler.sendEmptyMessage(MSG_LOAD_DATA);
-
-            mGoogleApiClient = new GoogleApiClient.Builder(mContext)
-                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                        @Override
-                        public void onConnected(Bundle connectionHint) {
-                            Log.e("Error", "onConnected: " + connectionHint);
-                            // Now you can use the Data Layer API
-                        }
-                        @Override
-                        public void onConnectionSuspended(int cause) {
-                            Log.e("Error", "onConnectionSuspended: " + cause);
-                        }
-                    })
-                    .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
-                        @Override
-                        public void onConnectionFailed(ConnectionResult result) {
-                            Log.e("Error", "onConnectionFailed: " + result);
-                        }
-                    })
-                    // Request access only to the Wearable API
-                    .addApi(Wearable.API)
-                    .build();
 
         }
 
@@ -473,6 +459,28 @@ public class MyWatchFace extends CanvasWatchFaceService {
                     mWakeLock = null;
                 }
             }
+        }
+
+
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+
+        @Override
+        public void onDataChanged(DataEventBuffer dataEventBuffer) {
+
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
         }
     }
 }
